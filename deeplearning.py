2d6a25a01bd44fbccf4626f7f0e6c4427c95f7f3
medo <mohamed@farghal.com>
2016-06-02 00:13:01 +0200

    ensumbling trials

--- deeplearning.py
+++ deeplearning.py
@@ -14,6 +14,36 @@ from keras.layers.core import *
 from keras.optimizers import Adadelta
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn import decomposition, pipeline, metrics, grid_search
+from sklearn.linear_model import LinearRegression
+from keras.optimizers import SGD 
+
+
+
+def encode_onehot(df, cols):
+    """
+    One-hot encoding is applied to columns specified in a pandas DataFrame.
+    
+    Modified from: https://gist.github.com/kljensen/5452382
+    
+    Details:
+    
+    http://en.wikipedia.org/wiki/One-hot
+    http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.OneHotEncoder.html
+    
+    @param df pandas DataFrame
+    @param cols a list of columns to encode
+    @return a DataFrame with one-hot encoding
+    """
+    vec = DictVectorizer()
+    
+    vec_data = pd.DataFrame(vec.fit_transform(df[cols].to_dict(outtype='records')).toarray())
+    vec_data.columns = vec.get_feature_names()
+    vec_data.index = df.index
+    
+    df = df.drop(cols, axis=1)
+    df = pd.concat([df, vec_data], axis=1)
+    return df
+
 
 
 scores = ['precision', 'recall', 'f-score', 'support']
@@ -55,31 +85,98 @@ def cross_validation_report(cv, labels):
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-
+contract = pd.read_csv("contract_ref.csv")
 train_ready = train
 test_ready = test
 
-def create_model():
-    model = Sequential()
-    model.add(Dense(64, input_dim=5, init='uniform', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-    return model
 
-model = KerasClassifier(build_fn=create_model, nb_epoch=50)
+def convert_gender(g):
+        return {"Female": "F", "f": "F", "F": "F", "Male": "M", "m": "M", "M":"M",
+                "Not Entered":"Unknown","Unknown":"Unknown"}[g]
+contract["GENDER"] = contract["GENDER"].apply(convert_gender)
+contract["VALUE_SEGMENT"][pd.isnull(contract["VALUE_SEGMENT"])] = "Unknown"
+contract["HANDSET_NAME"][pd.isnull(contract["HANDSET_NAME"])] = "Other"
+contract["AGE"][contract["AGE"] < 10] = 99
+contract["AGE"][contract["AGE"] == 99] = contract["AGE"].median()
+#contract["APPLE"] = contract["HANDSET_NAME"].apply(lambda x: x.lower().count("apple") > 0)
+#contract = contract.drop(["HANDSET_NAME"], axis=1)
+def convert_segment(s):
+    return {"Core": 1, "Med-Low": 2, "Med-High": 3, "High":4,
+           "Premium": 5, "Platinum": 6, "Unknown": 0}[s]
+contract["VALUE_SEGMENT"] = contract["VALUE_SEGMENT"].apply(convert_segment)
+contract = encode_onehot(contract, ["GENDER"])
+contract["AGE_UNKNOWN"] = contract["AGE"] == 99
+contract = encode_onehot(contract, ["HANDSET_NAME"])
+# contract["APPLE"] = contract["HANDSET_NAME"].apply(lambda x: x.lower().count("apple") > 0)
+# contract["GALAXY"] = contract["HANDSET_NAME"].apply(lambda x: x.lower().count("galaxy") > 0)
+contract = contract.drop(["RATE_PLAN"], axis=1)
 
-q = train_ready[["206_USAGE","207_USAGE","208_USAGE","209_USAGE","210_USAGE","TARGET", "CONTRACT_KEY"]]
+
+def feature_engineer_usage(ds):
+    global contract
+    cols = ["206_USAGE", "207_USAGE", "208_USAGE", "209_USAGE", "210_USAGE"]
+    s_cols = ["206_SESSION_COUNT", "207_SESSION_COUNT", "208_SESSION_COUNT", "209_SESSION_COUNT", "210_SESSION_COUNT"]
+    ds["211_USAGE"] = 0
+    ds["211_SESSION_COUNT"] = 0
+    ds["tmp"] = ''
+    ds["s_tmp"] = ''
+    def predict(s, m):
+        vals = map(int, s.split(",")[:-1])
+        reg = LinearRegression()
+        t = np.array(range(0, 5))
+        t.shape = (len(t), 1)
+        reg.fit(t, vals)
+        return reg.predict(m)
+#         return np.poly1d(np.polyfit(t, vals, 3))(6)
+    for i in cols: ds['tmp'] += ds[i].astype('string') + ','
+    for i in s_cols: ds['s_tmp'] += ds[i].astype('string') + ','
+    
+    ds['211_USAGE'] = ds.apply(lambda row: predict(row['tmp'], 6), axis=1)
+    ds['211_SESSION_COUNT'] = ds.apply(lambda row: predict(row['s_tmp'], 6), axis=1)
+    ds["MEAN"] = 0
+    ds["s_MEAN"] = 0
+    for i in cols: ds["MEAN"] += ds[i]
+    for i in s_cols: ds["s_MEAN"] += ds[i]
+    ds["MEAN"] /= 5.0
+    ds["s_MEAN"] /= 5.0
+#     df = train[["CONTRACT_KEY", "211_USAGE", "MEAN","TARGET", "210_USAGE"]]
+    df = ds
+    df = ds.drop(["tmp","s_tmp", "MEAN", "s_MEAN"], axis=1)
+#     df.merge(contract)
+    return df
+
+train_ready = feature_engineer_usage(train)
+test_ready = feature_engineer_usage(test)
+train_ready.merge(contract)
+
+
+q = train_ready
 # q["TARGET"] = q["TARGET"].apply(lambda x: True if x == 1 else False)
 #grid = GridSearchCV(estimator=model, param_grid=dict(n_estimators=ns))
 X = q[(q.keys().difference(['TARGET', "CONTRACT_KEY"]))].values
 y = q["TARGET"].values
 #y.shape = (len(y), 1)
+
+
+
+def create_model():
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model = Sequential()
+    model.add(Dense(100, input_dim=X.shape[1], init='normal', activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+    return model
+
+model = KerasClassifier(build_fn=create_model, nb_epoch=500, batch_size=100)
 
 
 # svd = TruncatedSVD(n_components=200)
